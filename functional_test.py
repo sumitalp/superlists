fff7b566bcfc29067f6fa1347442010252a416e6
Ahsanuzzaman Khan <webdevahsan@gmail.com>
2014-12-29 22:31:59 +0600

    Refactor for new method

--- functional_test.py
+++ functional_test.py
@@ -3,47 +3,58 @@ from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
-  
-  def setUp(self):
-      self.browser = webdriver.Firefox()
-      self.browser.implicitly_wait(5)
 
-  def tearDown(self):
-      self.browser.quit()
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(5)
 
-  def test_can_start_a_list_and_retrieve_it_later(self):
-      self.browser.get('http://localhost:8000')
-      self.assertIn('To-Do', self.browser.title)
-      header_text = self.browser.find_element_by_tag_name('h1')
+    def tearDown(self):
+        self.browser.quit()
 
-      #She is invited to enter a to-do item straight away
-      inputbox = self.browser.find_element_by_id('id_new_input')
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1')
 
-      self.assertEqual(
+        #She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_input')
+
+        self.assertEqual(
           inputbox.get_attribute('placeholder'),
           'Enter a to-do item'
-      )
+        )
 
-      # She types 'Buy peacock feathers' into a text box (Edith's hobby is tying fly-fishing lures)
-      inputbox.send_keys('Buy peacock feathers')
+        # She types 'Buy peacock feathers' into a text box (Edith's hobby is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
-      # When she hits enter, the page updates, and now the page lists
-      # '1: Buy peacock feathers' as an item in a to-do list table
-      inputbox.send_keys(Keys.ENTER)
+        # When she hits enter, the page updates, and now the page lists
+        # '1: Buy peacock feathers' as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
 
-      table = self.browser.find_element_by_id('id_list_table')
-      rows = table.find_elements_by_tag_name('tr')
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
-      self.assertTrue(
-          any(row.text == '1: Buy peacock feathers' for row in rows),
-          'New to-do item did not appear in table'
-      )
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
 
-      # There is still a text box inviting her to add another item. She enters
-      # 'Use peacock feathers to make a fly' (Edith is very methodical)
-      self.fail('Finish the test!')
+        #self.assertTrue(
+        #    any(row.text == '1: Buy peacock feathers' for row in rows),
+        #    'New to-do item did not appear in table -- its text was:\n%s' %(
+        #        table.text
+        #    )
+        #)
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make fly', [row.text for row in rows])
 
-      # The page updates again, and now shows both item on her list
+        # There is still a text box inviting her to add another item. She enters
+        # 'Use peacock feathers to make a fly' (Edith is very methodical)
+        self.fail('Finish the test!')
+
+        # The page updates again, and now shows both item on her list
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
